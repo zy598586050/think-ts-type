3c203a3b7d2577fd54cc2cf56c40084cfd0c85c8
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2023-10-25 11:41:09 +0800

    feature:【阶段性开发】

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-17 16:37:09
- * @LastEditTime: 2023-10-21 12:21:12
+ * @LastEditTime: 2023-10-24 19:10:59
  */
 declare module 'think-ts-lib' {
     export const start: (config?: Record<string, any>) => void
@@ -9,4 +9,15 @@ declare module 'think-ts-lib' {
     export interface RouteType {
         get: (url: string, str: string, middleware?: () => void) => void
     }
+    export interface Context {}
+    export class Controller {
+        ShowSuccess(data?: any, msg?: string, code?: number, statusCode?: number): Object
+        ApiException(msg?: string, errorCode?: number, statusCode?: number): void
+        GetParams(ctx: Context, validate: boolean): Object
+        View(url: string, data: Object, type?: string): string
+    }
+    export const ShowSuccess: (data?: any, msg?: string, code?: number, statusCode?: number) => Object
+    export const ApiException: (msg?: string, errorCode?: number, statusCode?: number) => void
+    export const GetParams: (ctx: Context, validate: boolean) => Object
+    export const View: (url: string, data: Object, type: string) => string
 }
