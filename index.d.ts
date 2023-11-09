af6a223457d9f13103ed21501d3d3755ecf757ef
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2023-11-09 21:29:34 +0800

    feature:【阶段提交】

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-17 16:37:09
- * @LastEditTime: 2023-10-24 19:10:59
+ * @LastEditTime: 2023-11-09 20:36:06
  */
 declare module 'think-ts-lib' {
     export const start: (config?: Record<string, any>) => void
@@ -19,5 +19,5 @@ declare module 'think-ts-lib' {
     export const ShowSuccess: (data?: any, msg?: string, code?: number, statusCode?: number) => Object
     export const ApiException: (msg?: string, errorCode?: number, statusCode?: number) => void
     export const GetParams: (ctx: Context, validate: boolean) => Object
-    export const View: (url: string, data: Object, type: string) => string
+    export const View: (url: string, data: Object, type?: string) => string
 }
