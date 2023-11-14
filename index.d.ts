3207fa46f5704b8a2a9e0faa6bacead15c68c0c7
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2023-11-14 20:12:10 +0800

    feature:【阶段提交】

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,9 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-17 16:37:09
- * @LastEditTime: 2023-11-13 11:06:30
+ * @LastEditTime: 2023-11-14 17:19:49
  */
+type LEVELS = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal'
 declare module 'think-ts-lib' {
     export const start: (config?: Record<string, any>) => void
     export const appConfig: Record<string, any>
@@ -20,9 +21,15 @@ declare module 'think-ts-lib' {
         ApiException(msg?: string, errorCode?: number, statusCode?: number): void
         GetParams(ctx: Context, validate?: boolean, url?: string): Object
         View(url: string, data?: Object, type?: string): string
+        Log4j(str: string, level?: LEVELS): void
+        Utils: any
+        M(modelPath: string): any
     }
     export const ShowSuccess: (data?: any, msg?: string, code?: number, statusCode?: number) => Object
     export const ApiException: (msg?: string, errorCode?: number, statusCode?: number) => void
     export const GetParams: (ctx: Context, validate?: boolean, url?: string) => Object
     export const View: (url: string, data?: Object, type?: string) => string
+    export const Log4j: (str: string, level?: LEVELS) => void
+    export const Utils: any
+    export const M: (modelPath: string) => any
 }
