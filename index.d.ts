7b9016f31376e6c5a7a0b9d43493943663b69139
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2023-11-23 14:33:18 +0800

    feature:【阶段提交】

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhangyu
  * @Date: 2023-10-17 16:37:09
- * @LastEditTime: 2023-11-15 18:24:09
+ * @LastEditTime: 2023-11-23 11:40:29
  */
 type LEVELS = 'trace' | 'debug' | 'info' | 'warn' | 'error' | 'fatal'
 declare module 'think-ts-lib' {
@@ -15,7 +15,12 @@ declare module 'think-ts-lib' {
         group: (prefix: string, callback: any, middleware?: any) => void
     }
     export type ExceptionType = (msg?: string, errorCode?: number, statusCode?: number) => void
-    export interface Context {}
+    export interface Context {
+        [key: string]: any
+    }
+    export interface PoolConnection {
+        [key: string]: any
+    }
     export class Controller {
         ShowSuccess(data?: any, msg?: string, code?: number, statusCode?: number): Object
         ApiException(msg?: string, errorCode?: number, statusCode?: number): void
@@ -25,6 +30,9 @@ declare module 'think-ts-lib' {
         Utils: any
         M(modelPath: string): any
         Db(tableName?: string, db?: string): any
+        EDb(db?: string): any
+        RDb(db?: string): any
+        MDb(modelName: string, db?: string): any
     }
     export const ShowSuccess: (data?: any, msg?: string, code?: number, statusCode?: number) => Object
     export const ApiException: (msg?: string, errorCode?: number, statusCode?: number) => void
@@ -34,4 +42,7 @@ declare module 'think-ts-lib' {
     export const Utils: any
     export const M: (modelPath: string) => any
     export const Db: (tableName?: string, db?: string) => any
+    export const EDb: (db?: string) => any
+    export const RDb: (db?: string) => any
+    export const MDb: (modelName: string, db?: string) => any
 }
